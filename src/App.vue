4ef88bce4f2691b93fa605ac67f7b3270c85d7ba
Imed Adel <aadelimed@gmail.com>
2019-06-17 16:35:57 +0200

    :sparkles: iterate and display predefined todos

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+	<div id="app">
+		<h1 class="text-5xl font-sans">{{ title }}</h1>
+		<ul>
+			<li v-for="todo in todos" class="flex" v-bind:key="todo.id">
+				{{ todo.text }}
+			</li>
+		</ul>
+		<img alt="Vue logo" src="./assets/logo.png" />
+		<HelloWorld msg="Welcome to Your Vue.js App" />
+	</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
-};
+	name: 'app',
+	data: function() {
+		return {
+			title: 'todoist',
+			todos: [
+				{
+					text: 'todo 1',
+					done: false,
+					id: Date.now(),
+				},
+				{
+					text: 'todo 2',
+					done: false,
+					id: Date.now() + 1,
+				},
+			],
+		}
+	},
+	methods: {},
+	components: {
+		HelloWorld,
+	},
+}
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
